251780709a1d053efa0dd524d60be37b1658cb09
linx1010 <49570637+linx1010@users.noreply.github.com>
2023-09-23 11:50:04 -0300

    Create load_data.py

---
+++ load_data.py
@@ -0,0 +1,40 @@
+from pymongo import MongoClient
+import pandas as pd
+folder = 'data/'
+client = MongoClient('mongodb://localhost:27017/')
+db = client['data_train']
+
+print('Remove coleções para evitar duplicidades eventuais duplicidades')
+
+# Verificar se a coleção existe
+colecoes = db.list_collection_names()
+for n in range(len(colecoes)):
+    db[colecoes[n]].drop()
+
+print('inserindo dados de treino')
+df = pd.read_csv(folder+'summaries_train.csv')
+collection = db['summaries_train']
+json_df = df.to_dict(orient='records')
+inserted_data = collection.insert_many(json_df)
+
+print('inserindo questoes de treino')
+df = pd.read_csv(folder+'prompts_train.csv')
+collection = db['prompts_train']
+json_df = df.to_dict(orient='records')
+inserted_data = collection.insert_many(json_df)
+
+print('inserindo questoes de teste')
+df = pd.read_csv(folder+'prompts_test.csv')
+collection = db['prompts_test']
+json_df = df.to_dict(orient='records')
+inserted_data = collection.insert_many(json_df)
+
+print('inserindo dados de teste')
+df = pd.read_csv(folder+'summaries_test.csv')
+collection = db['summaries_test']
+json_df = df.to_dict(orient='records')
+inserted_data = collection.insert_many(json_df)
+
+
+
+client.close()
